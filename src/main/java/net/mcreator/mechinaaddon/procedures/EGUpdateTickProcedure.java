99482474e17128122cd86db47cda4d061d92635c
neworld10 <luffy0630@gmail.com>
2023-08-15 01:33:11 +0900

    EG Timer

--- src/main/java/net/mcreator/mechinaaddon/procedures/EGUpdateTickProcedure.java
+++ src/main/java/net/mcreator/mechinaaddon/procedures/EGUpdateTickProcedure.java
@@ -1,13 +1,13 @@
 package net.mcreator.mechinaaddon.procedures;
 
-import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.energy.CapabilityEnergy;
 
+import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.block.BlockState;
 
-import net.mcreator.mechinaaddon.block.EGBlock;
 import net.mcreator.mechinaaddon.MechinaAddonMod;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -40,32 +40,63 @@ public class EGUpdateTickProcedure {
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
-		world.setBlockState(new BlockPos(x, y, z), EGBlock.block.getDefaultState(), 3);
 		if (new Object() {
-			public int getAmount(IWorld world, BlockPos pos, int sltid) {
-				AtomicInteger _retval = new AtomicInteger(0);
-				TileEntity _ent = world.getTileEntity(pos);
-				if (_ent != null) {
-					_ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
-						_retval.set(capability.getStackInSlot(sltid).getCount());
-					});
+			public double getValue(IWorld world, BlockPos pos, String tag) {
+				TileEntity tileEntity = world.getTileEntity(pos);
+				if (tileEntity != null)
+					return tileEntity.getTileData().getDouble(tag);
+				return -1;
+			}
+		}.getValue(world, new BlockPos(x, y, z), "timerTick") > 0) {
+			if (!world.isRemote()) {
+				BlockPos _bp = new BlockPos(x, y, z);
+				TileEntity _tileEntity = world.getTileEntity(_bp);
+				BlockState _bs = world.getBlockState(_bp);
+				if (_tileEntity != null)
+					_tileEntity.getTileData().putDouble("timerTick", ((new Object() {
+						public double getValue(IWorld world, BlockPos pos, String tag) {
+							TileEntity tileEntity = world.getTileEntity(pos);
+							if (tileEntity != null)
+								return tileEntity.getTileData().getDouble(tag);
+							return -1;
+						}
+					}.getValue(world, new BlockPos(x, y, z), "timerTick")) - 1));
+				if (world instanceof World)
+					((World) world).notifyBlockUpdate(_bp, _bs, _bs, 3);
+			}
+			if (new Object() {
+				public int getEnergyStored(IWorld world, BlockPos pos) {
+					AtomicInteger _retval = new AtomicInteger(0);
+					TileEntity _ent = world.getTileEntity(pos);
+					if (_ent != null)
+						_ent.getCapability(CapabilityEnergy.ENERGY, null).ifPresent(capability -> _retval.set(capability.getEnergyStored()));
+					return _retval.get();
 				}
-				return _retval.get();
+			}.getEnergyStored(world, new BlockPos(x, y - 1, z)) >= 100) {
+				{
+					TileEntity _ent = world.getTileEntity(new BlockPos(x, y - 1, z));
+					int _amount = (int) 100;
+					if (_ent != null)
+						_ent.getCapability(CapabilityEnergy.ENERGY, null).ifPresent(capability -> capability.receiveEnergy(_amount, false));
+				}
 			}
-		}.getAmount(world, new BlockPos(x, y, z), (int) (0)) > 0 && new Object() {
-			public int getEnergyStored(IWorld world, BlockPos pos) {
-				AtomicInteger _retval = new AtomicInteger(0);
-				TileEntity _ent = world.getTileEntity(pos);
-				if (_ent != null)
-					_ent.getCapability(CapabilityEnergy.ENERGY, null).ifPresent(capability -> _retval.set(capability.getEnergyStored()));
-				return _retval.get();
+		}
+		if (new Object() {
+			public double getValue(IWorld world, BlockPos pos, String tag) {
+				TileEntity tileEntity = world.getTileEntity(pos);
+				if (tileEntity != null)
+					return tileEntity.getTileData().getDouble(tag);
+				return -1;
 			}
-		}.getEnergyStored(world, new BlockPos(x, y, z)) >= 100) {
-			{
-				TileEntity _ent = world.getTileEntity(new BlockPos(x, y + 1, z));
-				int _amount = (int) 100;
-				if (_ent != null)
-					_ent.getCapability(CapabilityEnergy.ENERGY, null).ifPresent(capability -> capability.receiveEnergy(_amount, false));
+		}.getValue(world, new BlockPos(x, y, z), "timerTick") < 1) {
+			if (!world.isRemote()) {
+				BlockPos _bp = new BlockPos(x, y, z);
+				TileEntity _tileEntity = world.getTileEntity(_bp);
+				BlockState _bs = world.getBlockState(_bp);
+				if (_tileEntity != null)
+					_tileEntity.getTileData().putDouble("timerTick", 200);
+				if (world instanceof World)
+					((World) world).notifyBlockUpdate(_bp, _bs, _bs, 3);
 			}
 		}
 	}
