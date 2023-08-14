7ad049803187cf3db369085cde80d35c0448ea80
neworld10 <luffy0630@gmail.com>
2023-08-15 00:52:38 +0900

    EG

---
+++ src/main/java/net/mcreator/mechinaaddon/procedures/EGUpdateTickProcedure.java
@@ -0,0 +1,10 @@
+package net.mcreator.mechinaaddon.procedures;
+
+import java.util.Map;
+
+public class EGUpdateTickProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
