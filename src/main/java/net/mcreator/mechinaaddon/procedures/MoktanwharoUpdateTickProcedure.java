f3d3dbb318e90cee62bd4f31098da5ebe6afacae
neworld10 <luffy0630@gmail.com>
2023-08-15 02:02:16 +0900

    complete im gonna sleep now

---
+++ src/main/java/net/mcreator/mechinaaddon/procedures/MoktanwharoUpdateTickProcedure.java
@@ -0,0 +1,72 @@
+package net.mcreator.mechinaaddon.procedures;
+
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.CapabilityItemHandler;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.mechinaaddon.MechinaAddonMod;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.Map;
+
+public class MoktanwharoUpdateTickProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				MechinaAddonMod.LOGGER.warn("Failed to load dependency world for procedure MoktanwharoUpdateTick!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				MechinaAddonMod.LOGGER.warn("Failed to load dependency x for procedure MoktanwharoUpdateTick!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				MechinaAddonMod.LOGGER.warn("Failed to load dependency y for procedure MoktanwharoUpdateTick!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				MechinaAddonMod.LOGGER.warn("Failed to load dependency z for procedure MoktanwharoUpdateTick!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		if ((new Object() {
+			public ItemStack getItemStack(BlockPos pos, int sltid) {
+				AtomicReference<ItemStack> _retval = new AtomicReference<>(ItemStack.EMPTY);
+				TileEntity _ent = world.getTileEntity(pos);
+				if (_ent != null) {
+					_ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
+						_retval.set(capability.getStackInSlot(sltid).copy());
+					});
+				}
+				return _retval.get();
+			}
+		}.getItemStack(new BlockPos(x, y, z), (int) (0))).getItem() == Blocks.OAK_LOG.asItem()) {
+			{
+				TileEntity _ent = world.getTileEntity(new BlockPos(x, y, z));
+				if (_ent != null) {
+					final int _sltid = (int) (1);
+					final ItemStack _setstack = new ItemStack(Items.CHARCOAL);
+					_setstack.setCount((int) 1);
+					_ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
+						if (capability instanceof IItemHandlerModifiable) {
+							((IItemHandlerModifiable) capability).setStackInSlot(_sltid, _setstack);
+						}
+					});
+				}
+			}
+		}
+	}
+}
