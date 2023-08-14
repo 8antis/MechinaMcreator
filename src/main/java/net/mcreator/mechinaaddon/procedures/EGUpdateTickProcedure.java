ba5fed9030e45db4cfa5ca2c852a3bcbcaca0356
neworld10 <luffy0630@gmail.com>
2023-08-15 01:14:29 +0900

    EGEG

--- src/main/java/net/mcreator/mechinaaddon/procedures/EGUpdateTickProcedure.java
+++ src/main/java/net/mcreator/mechinaaddon/procedures/EGUpdateTickProcedure.java
@@ -1,10 +1,72 @@
 package net.mcreator.mechinaaddon.procedures;
 
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.energy.CapabilityEnergy;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.tileentity.TileEntity;
+
+import net.mcreator.mechinaaddon.block.EGBlock;
+import net.mcreator.mechinaaddon.MechinaAddonMod;
+
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.Map;
 
 public class EGUpdateTickProcedure {
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
-
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				MechinaAddonMod.LOGGER.warn("Failed to load dependency world for procedure EGUpdateTick!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				MechinaAddonMod.LOGGER.warn("Failed to load dependency x for procedure EGUpdateTick!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				MechinaAddonMod.LOGGER.warn("Failed to load dependency y for procedure EGUpdateTick!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				MechinaAddonMod.LOGGER.warn("Failed to load dependency z for procedure EGUpdateTick!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		world.setBlockState(new BlockPos(x, y, z), EGBlock.block.getDefaultState(), 3);
+		if (new Object() {
+			public int getAmount(IWorld world, BlockPos pos, int sltid) {
+				AtomicInteger _retval = new AtomicInteger(0);
+				TileEntity _ent = world.getTileEntity(pos);
+				if (_ent != null) {
+					_ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
+						_retval.set(capability.getStackInSlot(sltid).getCount());
+					});
+				}
+				return _retval.get();
+			}
+		}.getAmount(world, new BlockPos(x, y, z), (int) (0)) > 0 && new Object() {
+			public int getEnergyStored(IWorld world, BlockPos pos) {
+				AtomicInteger _retval = new AtomicInteger(0);
+				TileEntity _ent = world.getTileEntity(pos);
+				if (_ent != null)
+					_ent.getCapability(CapabilityEnergy.ENERGY, null).ifPresent(capability -> _retval.set(capability.getEnergyStored()));
+				return _retval.get();
+			}
+		}.getEnergyStored(world, new BlockPos(x, y, z)) >= 100) {
+			{
+				TileEntity _ent = world.getTileEntity(new BlockPos(x, y + 1, z));
+				int _amount = (int) 100;
+				if (_ent != null)
+					_ent.getCapability(CapabilityEnergy.ENERGY, null).ifPresent(capability -> capability.receiveEnergy(_amount, false));
+			}
+		}
 	}
 }
