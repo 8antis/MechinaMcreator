a66e9b053682d7bbdddb49cbf359cade7c30c7fb
USER <USER@DESKTOP-FMHVGP1>
2023-08-15 16:16:42 +0900

    MCreator Merge commit 'f3d3dbb318e90cee62bd4f31098da5ebe6afacae' into f7536aae737f54ac8545878c54c93fe1879c97da

--- src/main/java/net/mcreator/mechinaaddon/procedures/InputsignalblockBlockDestroyedByPlayerProcedure.java
+++ src/main/java/net/mcreator/mechinaaddon/procedures/InputsignalblockBlockDestroyedByPlayerProcedure.java
@@ -1,10 +1,11 @@
 package net.mcreator.mechinaaddon.procedures;
 
-import java.util.Map;
+import net.minecraftforge.eventbus.api.Event;
 
 public class InputsignalblockBlockDestroyedByPlayerProcedure {
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
 
 	}
+
 }
