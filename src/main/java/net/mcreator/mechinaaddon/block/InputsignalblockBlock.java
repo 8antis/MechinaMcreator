f7536aae737f54ac8545878c54c93fe1879c97da
8antis <USER@DESKTOP-FMHVGP1>
2023-08-15 16:16:06 +0900

    Test

--- src/main/java/net/mcreator/mechinaaddon/block/InputsignalblockBlock.java
+++ src/main/java/net/mcreator/mechinaaddon/block/InputsignalblockBlock.java
@@ -167,10 +167,7 @@ public class InputsignalblockBlock extends MechinaAddonModElements.ModElement {
 			int y = pos.getY();
 			int z = pos.getZ();
 
-			InputsignalblockBlockDestroyedByPlayerProcedure.executeProcedure(Stream
-					.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("x", x), new AbstractMap.SimpleEntry<>("y", y),
-							new AbstractMap.SimpleEntry<>("z", z))
-					.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
+			InputsignalblockBlockDestroyedByPlayerProcedure.executeProcedure(Collections.emptyMap());
 			return retval;
 		}
 
--- src/main/java/net/mcreator/mechinaaddon/procedures/InputsignalblockBlockDestroyedByPlayerProcedure.java
+++ src/main/java/net/mcreator/mechinaaddon/procedures/InputsignalblockBlockDestroyedByPlayerProcedure.java
@@ -1,64 +1,10 @@
 package net.mcreator.mechinaaddon.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import java.util.Map;
 
 public class InputsignalblockBlockDestroyedByPlayerProcedure {
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
-		if (dependencies.get("world") == null) {
-			if (!dependencies.containsKey("world"))
-				MechinaAddonMod.LOGGER.warn("Failed to load dependency world for procedure InputsignalblockBlockDestroyedByPlayer!");
-			return;
-		}
-		if (dependencies.get("x") == null) {
-			if (!dependencies.containsKey("x"))
-				MechinaAddonMod.LOGGER.warn("Failed to load dependency x for procedure InputsignalblockBlockDestroyedByPlayer!");
-			return;
-		}
-		if (dependencies.get("y") == null) {
-			if (!dependencies.containsKey("y"))
-				MechinaAddonMod.LOGGER.warn("Failed to load dependency y for procedure InputsignalblockBlockDestroyedByPlayer!");
-			return;
-		}
-		if (dependencies.get("z") == null) {
-			if (!dependencies.containsKey("z"))
-				MechinaAddonMod.LOGGER.warn("Failed to load dependency z for procedure InputsignalblockBlockDestroyedByPlayer!");
-			return;
-		}
 
-		IWorld world = (IWorld) dependencies.get("world");
-		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
-		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
-		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
-
-		if (RemoteRedstoneBlockBlock.block.getDefaultState().isValidPosition(world, new BlockPos(new Object() {
-			public double getValue(IWorld world, BlockPos pos, String tag) {
-				TileEntity tileEntity = world.getTileEntity(pos);
-				if (tileEntity != null)
-					return tileEntity.getTileData().getDouble(tag);
-				return -1;
-			}
-		}.getValue(world, new BlockPos(x, y, z), "RemoteBlockLocateX"), new Object() {
-			public double getValue(IWorld world, BlockPos pos, String tag) {
-				TileEntity tileEntity = world.getTileEntity(pos);
-				if (tileEntity != null)
-					return tileEntity.getTileData().getDouble(tag);
-				return -1;
-			}
-		}.getValue(world, new BlockPos(x, y, z), "RemoteBlockLocateY"), new Object() {
-			public double getValue(IWorld world, BlockPos pos, String tag) {
-				TileEntity tileEntity = world.getTileEntity(pos);
-				if (tileEntity != null)
-					return tileEntity.getTileData().getDouble(tag);
-				return -1;
-			}
-		}.getValue(world, new BlockPos(x, y, z), "RemoteBlockLocateZ"))) == false) {
-			if (world instanceof World) {
-				Block.spawnDrops(world.getBlockState(new BlockPos(x, y, z)), (World) world, new BlockPos(x, y, z));
-
-				world.destroyBlock(new BlockPos(x, y, z), false);
-			}
-		}
 	}
-
 }
